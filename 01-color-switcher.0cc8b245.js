66a0a5f2383dc17a5be32b2554d095dc3db3ae17
Melaverde1987 <79062250+Melaverde1987@users.noreply.github.com>
2023-08-20 00:01:31 +0000

    Deploying to gh-pages from @ Melaverde1987/goit-js-hw-09@7d804beef366df8cee4e6a3cfdd967c26a9ad50c 🚀

---
+++ 01-color-switcher.0cc8b245.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");let a=null;t.addEventListener("click",(function(t){t.target.setAttribute("disabled",""),a=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.0cc8b245.js.map
---
+++ 01-color-switcher.adc9c802.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body"),n=null;t.addEventListener("click",(function(t){t.target.setAttribute("disabled",""),n=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.adc9c802.js.map
